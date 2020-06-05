af287ea3e5b4379356a4ef30dd0dbecbc344125b
Namdev Jagtap <namdevjagtap30@gmail.com>
2020-06-05 19:28:19 +0530

    initial commit

--- plugins/cordova-plugin-crop/src/ios/CTCrop.h
+++
@@ -1,6 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-#import "PECropViewController.h"
-
-@interface CTCrop : CDVPlugin <PECropViewControllerDelegate>
-- (void) cropImage:(CDVInvokedUrlCommand *) command;
-@end
